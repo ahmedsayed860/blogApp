c474087cc0d87d3a7717275f70ce8b43861e5042
Ahmed Mahamoud <ahmedsayed860@gmail.com>
2022-02-19 14:37:27 +0300

    quleque de modifaction pour verifier si le systeme d authentication est fonctionnel

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,34 @@
 from flask import render_template, redirect, url_for, flash
-from blog import app
+from blog import app, dbase
 from blog.forms import RegisterForm, LoginForm
+from blog.models import Users
+from werkzeug.security import generate_password_hash#, check_password_hash
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    users = Users.query.all()
+    return render_template('home.html', users=users)
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
-        flash('success', 'Vous etes bien rediriger au homepage')
+        username = form.username.data
+        email = form.email.data
+        password = form.passwd.data
+        user = Users(username=username, email=email,
+                     passwd=generate_password_hash(password))
+        dbase.session.add(user)
+        dbase.session.commit()
+        flash('success', 'Vous etes bien enregistrer!')
         return redirect(url_for('home'))
-
+    """else:
+        flash('danger', "Echec veuiller ressayer de vous enregister!")
+        return redirect(url_for('register'))
+    """
     return render_template('register.html', title='Register', form=form)
 
 
